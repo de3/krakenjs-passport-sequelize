e1c9d0ca136721c4880f0d6912bacce15398d0cb
Edi <edi.firmansyah@gmail.com>
2016-09-25 17:50:30 +0700

    update some login features:
    - user logged in cannot access login page , auto redirect to profile
    - add logout page
    - identation to 2 space

--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,17 @@
 'use strict';
 
 module.exports = function (router) {
+  router.get('/', function (req, res) {
+    res.render('index');
+  });
 
-    router.get('/', function (req, res) {
-        
-        
-        res.render('index');
-        
-        
-    });
+  router.get('/admin', function (req, res) {
+    res.send('ini admin');
+  });
 
-    router.get('/admin', function (req, res) {
-
-        res.send('ini admin');
-
-    });
+  router.get('/logout', function (req, res) {
+    req.logout('/');
+    res.redirect('/login');
+  });
 
 };
--- controllers/login/index.js
+++ controllers/login/index.js
@@ -1,33 +1,27 @@
 'use strict';
-
-
 const passport = require('passport');
-
 
 module.exports = function (router) {
 
-    /**
-     * Display the login page. We also want to display any error messages that result from a failed login attempt.
-     */
-    router.get('/', function (req, res) {
-        //Include any error messages that come from the login process.
-        res.render('login', { messages: req.flash('info') });
-    });
+  /**
+   * Display the login page. We also want to display any error messages that result from a failed login attempt.
+   */
+  router.get('/', function (req, res) {
+    //Include any error messages that come from the login process.
+    res.render('login', { messages: req.flash('info') });
+  });
 
-    /**
-     * Receive the login credentials and authenticate.
-     * Successful authentications will go to /profile or if the user was trying to access a secured resource, the URL
-     * that was originally requested.
-     *
-     * Failed authentications will go back to the login page with a helpful error message to be displayed.
-     */
-    router.post('/', 
-        passport.authenticate('local', {
-            successRedirect: '/profile',
-            failureRedirect: '/login',
-            failureFlash: true
-        }));
-
-
-
+  /**
+   * Receive the login credentials and authenticate.
+   * Successful authentications will go to /profile or if the user was trying to access a secured resource, the URL
+   * that was originally requested.
+   *
+   * Failed authentications will go back to the login page with a helpful error message to be displayed.
+   */
+  router.post('/', 
+    passport.authenticate('local', {
+        successRedirect: '/profile',
+        failureRedirect: '/login',
+        failureFlash: true
+    }));
 };
